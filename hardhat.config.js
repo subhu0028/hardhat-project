6fce8460aff966fb5583c8613b7cab60e011589f
subhu0028 <116936244+subhu0028@users.noreply.github.com>
2023-10-08 08:09:30 +0530

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,7 @@
+// Replace import statement with require
+const toolbox = require("@nomicfoundation/hardhat-toolbox");
+
+// Export the configuration object using module.exports
+module.exports = {
+	solidity: "0.8.21"
+};
